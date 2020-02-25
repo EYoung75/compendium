36ef316cccb37909140e9c63c18ebe67a47a2bad
EYoung75 <eyoung75.ey@gmail.com>
2020-02-24 20:07:38 -0600

    incorporated react router into file structure and refined navigation and home card contents

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from "react";
 import "./main.scss";
 import Header from "./components/header.jsx";
 import Home from "./pages/home.jsx";
+import { Switch, Route } from "react-router-dom";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Header/>
-        <Home/>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          {/* <Route path="/work" exact component={Work} />
+        <Route path="/about" exact component={About} /> */}
+        </Switch>
       </div>
     );
   }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,35 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div className="home">
       <div className="home__row">
-        <div className="home__row__card">
-          <h3>Bottled for your pleasure</h3>
-          <h2>Potted Autobiography</h2>
-          <img src={require("../assets/brain.png")}/>
+        <Link to="/" exact className="home__row__card">
+          <h3>In his own words</h3>
+          <h2>Derived Autobiography</h2>
+          <img src={require("../assets/brain.png")} />
           <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam
             neque maxime rem? Dolores vel eligendi voluptate expedita dicta,
             autem incidunt obcaecati ipsam aperiam quia eos voluptas doloribus
             commodi quasi ad!
           </p>
-        </div>
-        <div className="home__row__card">
-          <h3>Catalogued Actions</h3>
-          <h2>Exhaustive Archives</h2>
-          <img src={require("../assets/anchor.png")}/>
+        </Link>
+        <Link to="/about" className="home__row__card">
+          <h3>Catalogued Endeavors</h3>
+          <h2>Definitive Archives</h2>
+          <img src={require("../assets/anchor.png")} />
           <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam
             neque maxime rem? Dolores vel eligendi voluptate expedita dicta,
             autem incidunt obcaecati ipsam aperiam quia eos voluptas doloribus
             commodi quasi ad!
           </p>
-        </div>
-        <div className="home__row__card">
+        </Link>
+        <Link to="/contact" className="home__row__card">
           <h3>Mr. Young is currently receiving</h3>
           <h2>Drop a line</h2>
-          <img src={require("../assets/dragonfly.png")}/>
+          <img src={require("../assets/dragonfly.png")} />
 
           <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam
@@ -37,7 +38,7 @@ const Home = () => {
             autem incidunt obcaecati ipsam aperiam quia eos voluptas doloribus
             commodi quasi ad!
           </p>
-        </div>
+        </Link>
       </div>
     </div>
   );
